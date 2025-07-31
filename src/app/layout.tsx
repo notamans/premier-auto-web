18b9e1ce2005ca1b1cebf6e9b682b2b417a51911
Aman Singh <amansinghs7010@gmail.com>
2025-07-31 13:13:38 -0300

    update chat widget

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,10 +36,10 @@ export default function RootLayout({
       <Script
         id="ixhello-chat-widget"
         type="module"
-        src="https://d3at15qgbg1tx6.cloudfront.net/chat-widget/assets/chat-widget.js"
+        src="https://d2i2haajno65nf.cloudfront.net/chat-widget/assets/chat-widget.js"
         data-org-id="095cf8eb-b79d-4542-9ae1-dbd9cce8ed28"
-        data-chat-widget-id="f16f74f6-c3c2-47c2-a1ac-deb58ba351bb"
-        data-flow-id="b92102e3-dc88-4dc5-acad-4d89137c9f68"
+        data-chat-widget-id="903a1b49-340f-4048-936b-5fa47ca9f710"
+        data-flow-id="4720b0b2-ff21-4533-a18b-e3054d88ecb2"
       />
     </html>
   );
